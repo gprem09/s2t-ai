baf7822bdda8268226e7aed6bf673cf31f255725
Gnanavel Premnath <gpa21@sfu.ca>
2024-04-05 21:42:15 -0700

    Update project.py

--- src/project.py
+++ src/project.py
@@ -1,18 +1,24 @@
-import torch 
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer, Wav2Vec2Processor
+import torch
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Wav2Vec2Config
 import librosa
 
+model_path = '/Users/gprem/Desktop/s2t-ai/src/default.pt'
 audio_input = '/Users/gprem/Desktop/s2t-ai/dataset/wavs/LJ001-0001.wav'
 
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+config = Wav2Vec2Config.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC(config)
 
-input_audio, _ = librosa.load(audio_input, sr=16000)
+model.load_state_dict(torch.load(model_path))
+model.eval()
 
-input_values = tokenizer(input_audio, return_tensors="pt").input_values
-logits = model(input_values).logits
+audio, _ = librosa.load(audio_input, sr=16000)
+input_values = processor(audio, return_tensors="pt", sampling_rate=16000).input_values
+
+with torch.no_grad():
+    logits = model(input_values).logits
+
 predicted_ids = torch.argmax(logits, dim=-1)
-transcription = tokenizer.batch_decode(predicted_ids)[0]
+transcription = processor.batch_decode(predicted_ids)[0]
 
 print(transcription)
