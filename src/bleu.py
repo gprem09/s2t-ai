6cae29ae565bdab5c45f8fe59e13afcc74bfbd21
gpa21 <gpa21@sfu.ca>
2024-04-06 11:21:34 -0700

    update

--- src/bleu.py
+++ src/bleu.py
@@ -1,16 +1,23 @@
-import numpy as np
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from nltk import word_tokenize
 
 def compute_bleu(ground_truths, predictions):
-    scores = []
+    scores = {
+        'bleu': [],
+        'unigram': [],
+        'bigram': [],
+        'trigram': []
+    }
+    smoothing = SmoothingFunction().method1
+    
     for gt, pred in zip(ground_truths, predictions):
-        reference_tokens = word_tokenize(gt)
+        reference_tokens = [word_tokenize(gt)]
         candidate_tokens = word_tokenize(pred)
+        
+        scores['bleu'].append(sentence_bleu(reference_tokens, candidate_tokens, smoothing_function=smoothing))
+        scores['unigram'].append(sentence_bleu(reference_tokens, candidate_tokens, weights=(1, 0, 0, 0), smoothing_function=smoothing))
+        scores['bigram'].append(sentence_bleu(reference_tokens, candidate_tokens, weights=(0.5, 0.5, 0, 0), smoothing_function=smoothing))
+        scores['trigram'].append(sentence_bleu(reference_tokens, candidate_tokens, weights=(0.33, 0.33, 0.33, 0), smoothing_function=smoothing))
 
-        score = sentence_bleu([reference_tokens], candidate_tokens)
-        scores.append(score)
-        # print(f"sentence_bleu: {score * 100:.2f}")
-
-    average_bleu_score = np.mean(scores) * 100
-    print(f"BLEU Score: {average_bleu_score:.2f}")
+    averages = {key: sum(value) / len(value) * 100 for key, value in scores.items()}
+    print(f"scores: {averages}")
