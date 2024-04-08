5b7543050e131d57b295734f99cbc82afb53ac14
gpa21 <gpa21@sfu.ca>
2024-04-08 00:38:43 -0700

    update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sys
 sys.path.append('src')
 from speech import LiveAudioTranscriber, AudioProcessor
@@ -8,9 +9,9 @@ app = Flask(__name__)
 audio_processor = AudioProcessor()
 transcriber = LiveAudioTranscriber(audio_processor)
 
-@app.route('/transcribe', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def transcribe_audio():
-    return jsonify({"transcription": "Transcribed text goes here"})
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
