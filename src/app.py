8c1a010cbe182fd6ea8d213c094279b0a3c5830a
gpa21 <gpa21@sfu.ca>
2024-04-08 01:49:34 -0700

    update

--- src/app.py
+++ src/app.py
@@ -1,21 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
-import threading
-import speech
 
 app = Flask(__name__)
 CORS(app)
 
-audio_processor = speech.AudioProcessor()
-transcriber = speech.LiveAudioTranscriber(audio_processor)
-
-@app.route('/transcribe', methods=['GET','POST'])
-def transcribe_audio():
-    audio_data = request.json['audioData']
-    sample_rate = request.json['sampleRate']
-    
-    transcription = transcriber.audio_processor.transcribe(audio_data, sample_rate)
-    return jsonify({"transcription": transcription})
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8080)
